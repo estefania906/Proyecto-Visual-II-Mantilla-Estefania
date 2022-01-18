ae5c6cbc0a39c99eb3fad2c4becb89abb12fd10f
Estefania Mantilla <estefaniamantillagallegos@gmail.com>
2022-01-17 19:19:30 -0500

    Primer ejemplo

---
+++ Sistema de gestion/Modelo/Entidades/Configuracion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.NewFolder
+{
+    public class Configuracion
+    {
+        public int CreditoMaximo { get; set }
+        public string EscuelaNombre { get; set }
+        public float NotaMaxima { get; set }
+
+        //La relacion con periodos 
+        public Periodo PeriodoVigente { get; set }
+        
+        public int PeriodoId { get; set }
+        public float PesoNota1 { get; set }
+        public float PesoNota2 { get; set }
+        public float PesoNota3 { get; set }
+
+
+    }
+}
